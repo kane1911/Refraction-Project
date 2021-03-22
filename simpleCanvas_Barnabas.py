98d95b7d8c37c3ad51e524f4e99336912143e7e8
bjohnc1 <bjc1n20@soton.ac.uk>
2021-03-22 15:33:55 +0000

    Fixed domain value error
    Fixed domain value error, changed showwarning to showerror, minorly adjusted layout, started developing a dropdown menu

--- simpleCanvas_Barnabas.py
+++ simpleCanvas_Barnabas.py
@@ -6,6 +6,20 @@ import tkinter.messagebox
 root = tkinter.Tk()
 root.title("Calculating Angle of Refraction")
 
+# Attempt at creating a drop-down menu:
+
+# i_medium_label = tkinter.Label(root, text="Incidence Medium:")
+# i_medium_label.grid(row=5, column=1)
+
+# variable = tkinter.StringVar(root)
+# variable.set("Incidence Medium")
+# i_medium_input = tkinter.OptionMenu(root, variable, "Air", "Water", "Glass")
+# i_medium_input.grid(row=5, column=2)
+
+# i_med = tkinter.Label(root, text=variable.get())
+# i_med.grid(row=6,column=2)
+# ------------------------------------------
+
 incidence_label = tkinter.Label(root, text = "Incidence Angle:")
 incidence_label.grid(row = 1, column = 1, sticky = "E")
 incidence_input = tkinter.Spinbox(root, from_=0, to=90, wrap=True) # Spinbox only accepts from a certain range of inputs (0 to 90 degrees)
@@ -16,15 +30,20 @@ index_label.grid(row = 2, column = 1, sticky = "E")
 index_input = tkinter.Spinbox(root, from_=0.1, to=100, increment=0.1, wrap=True) # # Spinbox only accepts from a certain range of inputs of index of refraction (0 to 100 in increments of 0.1)
 index_input.grid(row = 2, column = 2)
 
+refraction_label = tkinter.Label(root, text="Angle of Refraction (degrees):")
+refraction_label.grid(row=4, column=1, sticky = "E")
+
 def refraction_angle(n=1.5,
                      incidence_angle=40):  # function has default values of index of refraction = 1.5 (air to glass) and incidence angle = 40 degrees
     """Returns the angle of refraction from n (the index of refraction between the two media)
     and the angle of incidence (measured in degrees)."""
 
-    r_angle = math.degrees(math.asin(math.sin(math.radians(
-        incidence_angle)) / n))  # rearranged form of Snell's law with angles converted as necessary. asin function works only for radians.
-
-    return round(r_angle, 2)  # rounds to 2 d.p.
+    try:
+        r_angle = math.degrees(math.asin(math.sin(math.radians(incidence_angle)) / n))  # rearranged form of Snell's law with angles converted as necessary. asin function works only for radians.
+        
+        return round(r_angle, 2)  # rounds to 2 d.p.
+    except ValueError:
+        tkinter.messagebox.showerror(title="Domain error", message="Invalid combination of incidence angle and index of refraction.")
 
 def main():
     """Gets user input, calls the refraction_angle function, and displays the output."""
@@ -33,16 +52,16 @@ def main():
     incidence_angle = incidence_input.get()
 
     if n.isalpha() or incidence_angle.isalpha(): # if either of the inputs are letters
-        tkinter.messagebox.showwarning(title="Letter(s) entered", message="Please enter valid numbers for the incidence angle and index of refraction.") # displays warning pop-up window
+        tkinter.messagebox.showerror(title="Letter(s) entered", message="Please enter valid numbers for the incidence angle and index of refraction.") # displays warning pop-up window
     
-    else: # if both inputs are digits of decimals
+    else: # if both inputs are digits or decimals
         try:
             output = refraction_angle(float(n), float(incidence_angle))
 
             calc = tkinter.Label(root, text = output)
-            calc.grid(row = 3, column = 3)
+            calc.grid(row = 4, column = 2)
         except ValueError:
-            tkinter.messagebox.showwarning(title="Space or symbol entered", message="Invalid input.") # displays warning pop-up window
+            tkinter.messagebox.showerror(title="Space or symbol entered", message="Invalid input.") # displays warning pop-up window
     
 
 
