f5d4eda9b1400fb4794e5181709e64d6156ae195
bjohnc1 <bjc1n20@soton.ac.uk>
2021-04-19 10:09:52 +0100

    Update graphics_Barnabas_1.py
    Integrated the canvas into main code. The canvas also now responds to angle changes, but there are bugs (like with negative angles).

--- graphics_Barnabas_1.py
+++ graphics_Barnabas_1.py
@@ -5,7 +5,7 @@ import tkinter.messagebox
 
 root = tkinter.Tk()
 root.title("Group 19: Angle of Refraction Calculator")
-root.geometry("400x400") # window size
+root.geometry("900x600") # window size
 
 
 def refraction_angle(n, incidence_angle):
@@ -61,63 +61,115 @@ def main():
         else: # if both inputs are digits or decimals between -90° and 90°
             r_angle = refraction_angle(n, float(incidence_angle))
 
-            calculated_angle = tkinter.Label(root, text = str(r_angle) + "°") # concatenates degree symbol to
+            calculated_angle = tkinter.Label(frame, text = str(r_angle) + "°") # concatenates degree symbol to
             calculated_angle.grid(row = 5, column = 2)
 
     except ValueError:
         tkinter.messagebox.showerror(title="Space or symbol entered", message="Invalid input.") # displays warning pop-up window
     
+    # -------------Draws laser on canvas -------------
+    my_canvas.create_line(ind_laser_x(float(incidence_input.get())), ind_laser_y(float(incidence_input.get())), 250, 250) # incidence ray ending at our origin (250,250)
+    
+    my_canvas.create_line(250, 250, refr_laser_x(r_angle), refr_laser_y(r_angle)) # refracted ray beginning at our origin (0,0)
+    
     # ----------Widgets showing additional calculations that only display when the Calculate button is pressed:
     
-    index_label = tkinter.Label(root, text="Refractive index from {} to {}:".format(indexmedium1.get(), indexmedium2.get())) 
+    index_label = tkinter.Label(frame, text="Refractive index from {} to {}:".format(indexmedium1.get(), indexmedium2.get())) 
     index_label.grid(row=6, column=1)
 
-    n_label = tkinter.Label(root, text=str(n)) # displays the index of refractin between the two selected media
+    n_label = tkinter.Label(frame, text=str(n)) # displays the index of refractin between the two selected media
     n_label.grid(row=6, column=2)
 
-    i_medium_v_label = tkinter.Label(root, text="Velocity of light in {}:".format(indexmedium1.get())) 
+    i_medium_v_label = tkinter.Label(frame, text="Velocity of light in {}:".format(indexmedium1.get())) 
     i_medium_v_label.grid(row=7, column = 1, sticky = "E")
 
-    i_medium_v = tkinter.Label(root, text= str(find_v(find_abs_n(indexmedium1.get()))) + " m/s") # displays the velocity of light in the incidence medium based on its absolute refractive index
+    i_medium_v = tkinter.Label(frame, text= str(find_v(find_abs_n(indexmedium1.get()))) + " m/s") # displays the velocity of light in the incidence medium based on its absolute refractive index
     i_medium_v.grid(row=7, column = 2)
 
-    i_medium_v_label = tkinter.Label(root, text="Velocity of light in {}:".format(indexmedium2.get())) 
+    i_medium_v_label = tkinter.Label(frame, text="Velocity of light in {}:".format(indexmedium2.get())) 
     i_medium_v_label.grid(row=8, column = 1, sticky = "E")
 
-    i_medium_v = tkinter.Label(root, text= str(find_v(find_abs_n(indexmedium2.get()))) + " m/s") # displays the velocity of light in the refraction medium based on its absolute refractive index
+    i_medium_v = tkinter.Label(frame, text= str(find_v(find_abs_n(indexmedium2.get()))) + " m/s") # displays the velocity of light in the refraction medium based on its absolute refractive index
     i_medium_v.grid(row=8, column = 2)
 
+    
 
 
-# -------------Window widgets and grid layout:   
+# -------------Window widgets and in frame:   
 
-incidence_label = tkinter.Label(root, text = "Incidence Angle (°):")
+frame = tkinter.Frame(root) # creates a frame for the other widgets. see: https://stackoverflow.com/questions/20149483/python-canvas-and-grid-tkinter 
+frame.grid(row=0,column=0, sticky="n") # sticky = "n" moves aligns widgets to the top of the window
+
+incidence_label = tkinter.Label(frame, text = "Incidence Angle (°):")
 incidence_label.grid(row = 1, column = 1, sticky = "E")
-incidence_input = tkinter.Spinbox(root, from_=-90, to=90, wrap=True) # Spinbox only accepts from a certain range of inputs (-90 to 90 degrees)
+incidence_input = tkinter.Spinbox(frame, from_=-90, to=90, wrap=True) # Spinbox only accepts from a certain range of inputs (-90 to 90 degrees)
 incidence_input.grid(row = 1, column = 2)
 
-ttk.Label(text="Select Incidence Medium:").grid(row=2, column=1, sticky="E") # incidence medium drop down menu
+ttk.Label(frame, text="Select Incidence Medium:").grid(row=2, column=1, sticky="E") # incidence medium drop down menu
 n1 = tkinter.StringVar()
-indexmedium1 = ttk.Combobox(root, width=18, textvariable=n1)
+indexmedium1 = ttk.Combobox(frame, width=18, textvariable=n1)
 indexmedium1['values'] = ('Air', 'Glass', 'Water')
 indexmedium1['state'] = 'readonly' # means the user cannot directly enter a value into the combobx see:https://www.pythontutorial.net/tkinter/tkinter-combobox/
 indexmedium1.grid(row=2, column=2)
 indexmedium1.current(0) # Shows Air as default
 
-ttk.Label(text="Select Refraction Medium:").grid(row=3, column=1, sticky="E") # refraction medium drop down menu
+ttk.Label(frame, text="Select Refraction Medium:").grid(row=3, column=1, sticky="E") # refraction medium drop down menu
 n2 = tkinter.StringVar()
-indexmedium2 = ttk.Combobox(root, width=18, textvariable=n2)
+indexmedium2 = ttk.Combobox(frame, width=18, textvariable=n2)
 indexmedium2['values'] = ('Air', 'Glass', 'Water', 'Amber', 'Diamond')
 indexmedium2['state'] = 'readonly' # means the user cannot directly enter a value into the combobx see:https://www.pythontutorial.net/tkinter/tkinter-combobox/
 indexmedium2.grid(row=3, column=2)
 indexmedium2.current(1) # Shows Glass as default
 
 
-calculate_button = tkinter.Button(root, text = "Calculate", command = main) # Calculate button runs the main() function
+calculate_button = tkinter.Button(frame, text = "Calculate", command = main) # Calculate button runs the main() function
 calculate_button.grid(row = 4, column = 2)
 
-refraction_label = tkinter.Label(root, text="Angle of Refraction:")
+refraction_label = tkinter.Label(frame, text="Angle of Refraction:")
 refraction_label.grid(row=5, column=1, sticky = "E")
+
+# ------Canvas ------------------------------
+
+my_canvas = tkinter.Canvas(root, width = 500, height = 500, background = "white") # canvas
+my_canvas.grid(row = 0, column = 1)
+
+x1 = 250
+x2 = 250
+y1 = 0
+y2 = 500
+my_canvas.create_line(x1, y1, x2, y2) # y-axis
+
+x3 = 0
+x4 = 500
+y3 = 250
+y4 = 250
+my_canvas.create_line(x3, y3, x4, y4) # x-axis
+
+
+def ind_laser_x(ind_angle):
+    """Calcs incident angle laser x-coord"""
+    x = 250 - (math.sin(math.radians(ind_angle)) * 250) # switched cos to sin so the angle of incidence is relative to the normal
+    return x
+
+
+def ind_laser_y(ind_angle):
+    """Calcs incident angle laser y-coord"""
+    y = 250 - (math.cos(math.radians(ind_angle)) * 250) # switched to cos so the angle of incidence is relative to the normal
+    return y
+
+#my_canvas.create_line(ind_laser_x(40), ind_laser_y(40), 250, 250) # incidence ray ending at our origin (250,250)
+
+
+def refr_laser_x(refr_angle):
+    """Calcs refraction angle laser x-coord."""
+    x = 250 + (math.sin(math.radians(refr_angle)) * 250) # switched cos to sin so that the refraction angle is relative to the normal
+    return x
+
+def refr_laser_y(refr_angle):
+    """Calcs refraction angle laser y-coord."""
+    y = 250 + (math.cos(math.radians(refr_angle)) * 250) # switched cos to sin so that the refraction angle is relative to the normal
+    return y
+
 
 
 root.mainloop()
